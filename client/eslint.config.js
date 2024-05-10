d5a5bf6c054d194267e4e71b920a55aea1472e22
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2024-05-10 11:44:46 +0200

    eslint for svelte setup

---
+++ client/eslint.config.js
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...eslintPluginSvelte.configs["flat/recommended"],
+  {
+    rules: {
+      indent: ["error", 2],
+      "linebreak-style": ["error", "windows"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+    ignores: ["node_modules/", "dist/", "eslint.config.js", ".env", "src/main.js", "vite.config.js", "svelte.config.js" ],
+  },
+];
