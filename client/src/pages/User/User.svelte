26a63692f7ea71e26de10fb05f95aadd9b9d58a0
Wei Yang <weix0015@stud.kea.dk>
2024-05-25 13:08:00 +0200

    added delete user and roles: admin, user for frontend

--- client/src/pages/User/User.svelte
+++ client/src/pages/User/User.svelte
@@ -21,6 +21,23 @@
     }
   };
 
+  const deleteUser = async (email) => {
+    try {
+      const response = await fetch(`/api/users/${email}`, {
+        method: "DELETE"
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      // update users array
+      users = users.filter(user => user.email !== email);
+    } catch(error) {
+      console.error("Error deleting user:", error);
+    }
+  };
+
   onMount(fetchUsers);
 </script>
 
@@ -30,6 +47,8 @@
     <tr>
     <th>Username</th>
     <th>Email</th>
+    <th>Role</th>
+    <th>Actions</th>
   </tr>
   </thead>
   <tbody>
@@ -37,6 +56,15 @@
     <tr>
       <td>{user.username}</td>
       <td>{user.email}</td>
+      <td>{user.is_admin ? "Admin" : "User"}</td>
+      <td>
+        {#if !user.is_admin}
+        <button on:click={() => deleteUser(user.email)}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
+            <path d="M3 6l3 16.125A2.978 2.978 0 0 0 8.963 24h6.074a2.978 2.978 0 0 0 2.963-1.875L21 6H3zM14 3V2a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v1H5v2h14V3h-5zM10 2h4v1h-4V2zm1 7h2v10h-2V9zm-4 0h2v10H7V9zm10 0h-2v10h2V9z"/>
+        </button>
+        {/if}
+      </td>
     </tr>
     {/each}
   </tbody>
@@ -56,5 +84,18 @@
 
   th {
     background-color: #333;
+  }
+
+  button {
+    background-color: red;
+    color: white;
+    border: none;
+    padding: 5px 10px;
+    cursor: pointer;
+    border-radius: 4px;
+  }
+
+  button:hover {
+    background-color: darkred;
   }
 </style>
