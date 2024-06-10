101fc46d8318df36e326d794c1a59cbe28c4115b
Wei Yang <weix0015@stud.kea.dk>
2024-06-10 14:00:44 +0200

    removed params

--- client/src/util/PrivateRoute.svelte
+++ client/src/util/PrivateRoute.svelte
@@ -6,8 +6,8 @@
 	export let isAdmin = false;
 </script>
 
-<Route {path} let:params let:location let:navigate>
+<Route {path}>
 	<PrivateRouteGuard {isAdmin}>
-		<slot {params} {location} {navigate} />
+		<slot />
 	</PrivateRouteGuard>
 </Route>
